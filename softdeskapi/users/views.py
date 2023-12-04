da3a63da7c65820b70af567f9024c39dc981bc31
DayriseA_dom <adrien.maisak@gmail.com>
2023-12-04 04:07:52 +0100

    feat: add UserViewSet; add urls.py for users app
    update project urls.py to include users.urls

--- softdeskapi/users/views.py
+++ softdeskapi/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
