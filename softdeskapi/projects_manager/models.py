cc4a89b1b04d6c3c362c98a21b9088963acb532a
DayriseA_dom <adrien.maisak@gmail.com>
2023-12-05 20:03:18 +0100

    feat(projects_manager): add 'Project' (and his through table) model
    make and run corresponding migrations
    also create empty (for now) models 'Issue' and 'Comment'

--- softdeskapi/projects_manager/models.py
+++ softdeskapi/projects_manager/models.py
@@ -1,3 +1,56 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    """Project model."""
+
+    PROJECT_TYPES = [
+        ("backend", "Back-end"),
+        ("frontend", "Front-end"),
+        ("ios", "iOS"),
+        ("android", "Android"),
+    ]
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=20, choices=PROJECT_TYPES)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name="projects_created",
+        null=True,
+    )
+    contributors = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through="Contributor",
+        related_name="contributing_projects",
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return project name."""
+        return self.name
+
+
+class Contributor(models.Model):
+    """Custom through model for contributors."""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return user and project name."""
+        return f"{self.user.username} - {self.project.name}"
+
+
+class Issue(models.Model):
+    """Issue model."""
+
+    pass
+
+
+class Comment(models.Model):
+    """Comment model."""
+
+    pass
