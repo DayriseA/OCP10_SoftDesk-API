c44060cb4a942e4f5a67118f30ea138bae4a0f93
DayriseA_dom <adrien.maisak@gmail.com>
2023-12-12 01:44:08 +0100

    feat(projects_manager): add Issue model; make migrations and migrate

--- softdeskapi/projects_manager/models.py
+++ softdeskapi/projects_manager/models.py
@@ -43,3 +43,54 @@ class Contributor(models.Model):
     def __str__(self):
         """Return user and project name."""
         return f"{self.user.username} - {self.project.name}"
+
+
+class Issue(models.Model):
+    """Issue model."""
+
+    ISSUE_TYPES = [
+        ("bug", "BUG"),
+        ("feature", "FEATURE"),
+        ("task", "TASK"),
+    ]
+
+    PRIORITIES = [
+        ("low", "LOW"),
+        ("medium", "MEDIUM"),
+        ("high", "HIGH"),
+    ]
+
+    STATUS = [
+        ("todo", "To Do"),
+        ("in_progress", "In Progress"),
+        ("finished", "Finished"),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=10, choices=ISSUE_TYPES)
+    priority = models.CharField(max_length=10, choices=PRIORITIES)
+    status = models.CharField(max_length=15, choices=STATUS)
+
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="issues"
+    )
+
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        related_name="issues_assigned",
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name="issues_created",
+        null=True,
+    )
+
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return issue name."""
+        return self.name
